9283ce4a3aed983e0d2b1f395a94ae85126dee27
djsamkidd <33381106+djsamkidd@users.noreply.github.com>
2019-07-06 16:47:53 -0700

    update1

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -9,21 +9,21 @@ export default class Contact extends Component {
                 <div className="contact_container">
                     <div className="contact_content">
                     <form id="contactForm" method="post">
+        <h1>Contact Me</h1>
         <label className="label">Name:</label>
             <input type= "text" id="name-input" placeholder="First name.." required/>
 
         <label className="label">Email:</label>
             <input type="email"id="email-input"name="email"placeholder="Your email.." required/>
 
-        <label className="label">Phone:</label>
+        <label className="label">Phone:</label><br></br>
             <input type="phone"id="phone-input"name="phone"placeholder="Your phone number.." required/>
-
-        <label className="label">Message</label>
-            <textarea id="description-input" name="content" placeholder="Write something.." required>
+            <br></br><br></br>
+        <label className="label">Message:</label><br></br>
+            <textarea className="phone" id="description-input" name="content" placeholder="Write something.." required>
             </textarea>
-            <div class="g-recaptcha" data-sitekey="6Lc7cVMUAAAAAM1yxf64wrmO8gvi8A1oQ_ead1ys" class="form-control"></div>
-            <div id="toast"></div>
-            <button className="contact_button" onClick="submitToAPI(event)" type="submit" id="submit"><label className="label">Send</label></button>
+
+            <input type="submit" value="submit"/>
     </form>
                     </div>
                 </div>
